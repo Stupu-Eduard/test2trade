b49ebc3ab3007b06ad557a786634e831dce0bc40
Stupu-Eduard <eduard.stupu1@gmail.com>
2026-05-26 17:17:14 +0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/test2trade/test2trade/Test2tradeApplication.java
@@ -0,0 +1,13 @@
+package com.test2trade.test2trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2tradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2tradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test2trade/test2trade/Test2tradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test2trade.test2trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2tradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
